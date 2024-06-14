b69ce817cc74bb8c366b945d95fe675e3f31e3d9
Bhavya Shukla <65808829+bhavyashukla23@users.noreply.github.com>
2024-06-14 20:01:02 +0530

    Time: 36 ms (79.67%) | Memory: 60.2 MB (44.92%) - LeetSync

--- 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.java
+++ 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.java
@@ -1,13 +1,16 @@
 class Solution {
     public int minIncrementForUnique(int[] nums) {
-         Arrays.sort(nums);
-        int ctr = 0;
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] <= nums[i - 1]) {
-                ctr += (nums[i - 1] - nums[i] + 1);
-                nums[i] = nums[i - 1] + 1;
-            }
+          Arrays.sort(nums);
+
+        int numTracker = 0; // Tracks the next unique number that should be set.
+        int minIncrement = 0; // Counts the total increments required.
+
+        for (int num : nums) {
+            numTracker = Math.max(numTracker, num);
+            minIncrement += numTracker - num;
+            numTracker += 1; // Increment the tracker for the next number.
         }
-        return ctr;
+
+        return minIncrement;
     }
 }
